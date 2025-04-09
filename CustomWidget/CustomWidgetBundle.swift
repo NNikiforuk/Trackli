72e1ea4d0aa918e85f0a97a853a182cd3ed171e3
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-04-09 15:47:26 +0200

    Injecting WidgetKit

---
+++ CustomWidget/CustomWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  CustomWidgetBundle.swift
+//  CustomWidget
+//
+//  Created by Natalia Nikiforuk on 09/04/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CustomWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CustomWidget()
+        CustomWidgetControl()
+    }
+}
